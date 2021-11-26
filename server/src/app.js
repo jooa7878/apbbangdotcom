707869b22a14c28d1c2782fbcb3a0deb944b5706
jooa7878 <jooa7878@naver.com>
2021-11-27 01:08:09 +0900

    fix : err fix

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("build"));
+app.use(express.static("../build"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/build/index.html");
