68ac57fddf01961952c4eb54c2f38f57fd65a023
jooa7878 <jooa7878@naver.com>
2021-11-27 13:20:18 +0900

    changed to javascript

--- server/src/app.js
+++ server/src/app.js
@@ -8,16 +8,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("server is running");
+  console.log("server is running ~");
 });
-// 클라이언트에서 빌드하고 빌드 폴더를 서버에 옮김
-// 우리의 첫 위치는 안폰빵닷컴폴더
-// 클라이언트 폴더로 이동함
-// 클라이언트는 node_modules가 없음 nopm ci
-// npm run build
-// 클라이언트 빌드 폴더 서버에 옮김
-
-// 서버 폴더로 이동해서
-// npm ci
-// node add.js
-// 이 부분을 컴퓨터가 알아서 하면 지속적인 배포 가능
