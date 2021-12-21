7d6a8f9b4ed5c2ba651d716dc43d9559c12c3733
jooa7878 <jooa7878@naver.com>
2021-12-21 17:36:27 +0900

    feat : create page layout

--- client/src/components/Record.js
+++ client/src/components/Record.js
@@ -10,42 +10,10 @@ const dummyData_1on1 = [
   { id: 3, player_1: "형준", player_2: "창균", winner_player: 2 },
 ];
 
-const dummyData_league = [
-  {
-    id: 0,
-    player_1: ["안상", "감자", "창균", "승찬"],
-    player_2: ["원모", "준영", "형준", "지원"],
-    winner_player: 1,
-  },
-  {
-    id: 1,
-    player_1: ["안상", "감자", "창균", "승찬"],
-    player_2: ["원모", "준영", "형준", "지원"],
-    winner_player: 2,
-  },
-  {
-    id: 2,
-    player_1: ["안상", "감자", "창균", "승찬"],
-    player_2: ["원모", "준영", "형준", "지원"],
-    winner_player: 1,
-  },
-  {
-    id: 3,
-    player_1: ["안상", "감자", "창균", "승찬"],
-    player_2: ["원모", "준영", "형준", "지원"],
-    winner_player: 2,
-  },
-];
-
 function Record() {
-  const [flag, setFlag] = useState("1on1");
-
-  function rendering(flag) {
+  function rendering() {
     const renderList = [];
-    let list = [];
-
-    if (flag === "personal") list = dummyData_1on1;
-    else list = dummyData_league;
+    let list = dummyData_1on1;
 
     for (let i = 0; i < 4; i++) {
       renderList.push(
@@ -77,31 +45,8 @@ function Record() {
   return (
     <div className="record_container">
       <h2 className="record_title">최근 스타 경기</h2>
-      <div className="record_btn_container">
-        <button
-          className="btn selected"
-          onClick={(e) => {
-            document.querySelector(".selected").classList.remove("selected");
-            setFlag("1on1");
-            e.target.classList.add("selected");
-          }}
-        >
-          개인
-        </button>
-        <button
-          className="btn"
-          onClick={(e) => {
-            document.querySelector(".selected").classList.remove("selected");
-            setFlag("league");
-            e.target.classList.add("selected");
-          }}
-        >
-          리그
-        </button>
-      </div>
-      <ul className="record_list">
-        {flag === "1on1" ? rendering("personal") : rendering("league")}
-      </ul>
+      <div className="record_btn_container"></div>
+      <ul className="record_list">{rendering()}</ul>
       <Link to="/createrecord" class="record_link">
         전적 입력하러 가기
       </Link>
--- client/src/pages/CreateRecord.js
+++ client/src/pages/CreateRecord.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import "../style/CreateRecord.scss";
 
 function CreateRecord() {
-  return <h1>HelloWorld</h1>;
+  const [player_1, setPlayer_1] = useState();
+  const [player_2, setPlayer_2] = useState();
+  const [winner, setWinner] = useState();
+
+  function onSumbit(e) {
+    e.preventDefault();
+
+    if (checkInputs()) {
+      window.alert("입력 데이터 확인! 추후 기능 업데이트 예정입니다.");
+    }
+  }
+
+  function checkInputs() {
+    if (
+      player_1 === undefined ||
+      player_2 === undefined ||
+      winner === undefined ||
+      player_1 === "" ||
+      player_1.trim() === "" ||
+      player_2 === "" ||
+      player_2.trim() === "" ||
+      winner === "" ||
+      winner.trim() === ""
+    ) {
+      window.alert("입력이 안 된 사항이 있습니다.");
+      return false;
+    } else if (player_1 === player_2) {
+      window.alert("입력 값을 다시 확인해주세요.");
+      return false;
+    } else if (winner !== player_1 && winner !== player_2) {
+      window.alert("승자를 제대로 입력해주세요.");
+      return false;
+    }
+
+    return true;
+  }
+  return (
+    <div className="create_container">
+      <form className="form_container" onSubmit={onSumbit}>
+        <h1 className="form_title">경기 입력하기</h1>
+        <div className="input_container">
+          <label htmlFor="player_1" className="form_label">
+            플레이어 1
+          </label>
+          <input
+            type="text"
+            id="player_1"
+            className="form_input"
+            onChange={(e) => {
+              setPlayer_1(e.target.value);
+            }}
+          />
+        </div>
+        <div className="input_container">
+          <label htmlFor="player_2" className="form_label">
+            플레이어 2
+          </label>
+          <input
+            type="text"
+            id="player_2"
+            className="form_input"
+            onChange={(e) => {
+              setPlayer_2(e.target.value);
+            }}
+          />
+        </div>
+        <div className="input_container">
+          <label htmlFor="winner" className="form_label">
+            승자
+          </label>
+          <input
+            type="text"
+            id="winner"
+            className="form_input"
+            onChange={(e) => {
+              setWinner(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" className="btn_submit">
+          전송
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateRecord;
